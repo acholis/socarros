b07f227b5e15083202596d3e403828b9cc0fcf33
cabanga <manucabanga@gmail.com>
2020-07-22 18:34:43 +0100

    this commit config notificações

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,9 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +12,23 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+$(document).ready(function(){
+
+    if ( $('div.alert.fade.in').length ) {
+
+        $('div.alert.fade.in').addClass("box-notification");
+        $('div.alert.fade.in').addClass("box-notification-sombra");
+        $('div.alert.fade.in').addClass("animateOpen");
+    
+        $('button.close').addClass("hide");
+    
+        setTimeout(function(){
+            $(".box-notification.box-notification-sombra").removeClass("animateOpen");
+            $(".box-notification.box-notification-sombra").addClass("animateClose");
+            $(".box-notification.box-notification-sombra").remove();
+        }, 3000);
+    }
+
+})
