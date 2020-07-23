53c94a7b3214e0d82011fb2871c6f4c08d892e35
cabanga <manucabanga@gmail.com>
2020-07-23 13:56:20 +0100

    Thsi commit update colors default in web

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,37 @@
 include ActionView::Helpers::NumberHelper
-
 module ApplicationHelper
+    def meta_description
+        "Só carros é a sua plataforma digital que conecta os vendedores e compradores de carros novos e de ocasião 
+         de forma simples, rápida e segura. Garantimos para si comprador " 
+    end
+
+    def meta_title_fb
+        "Só carros é a sua plataforma digital que conecta os vendedores e compradores de carros novos e de ocasião 
+        de forma simples, rápida e segura"
+    end
+
+    def meta_keywords
+        "Acholis, Só carros, TeamBuild, Cabanga, Angola,
+        Barato, Caro, Simples, Facil, Seguro, Compra, compra seguro,
+        Carro, Carros usados, Carros, Carros baratos, Carros usados baratos, Carrossel antigo, Carros baratos usados
+        "
+    end
+
     Float.class_eval do
         # Converte floats para kwanza
         # deve ser chamado como método de instância
         def to_kwanza
-          valor = self
-          valor = 0 if valor.blank?
-          number_to_currency(valor, unit: "AKz", separator: ",", delimiter:".", format: "%n %u")
+            valor = self
+            valor = 0 if valor.blank?
+            number_to_currency(valor, unit: "AKz", separator: ",", delimiter:".", format: "%n %u")
         end
   
         def to_month
-          self.to_i.to_month
+            self.to_i.to_month
         end
   
         def to_month_abbr
-          self.to_i.to_month_abbr
+            self.to_i.to_month_abbr
         end
     end
 
@@ -58,4 +74,8 @@ module ApplicationHelper
           self.to_f.to_kwanza
         end
     end
+
+
+
+
 end
